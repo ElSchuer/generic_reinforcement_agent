3d5dc7394a33c6745b7153f92ec5309824f262d5
Lars Schürmann <schuermann.lars@gmail.com>
2019-01-15 11:38:37 +0100

    added mean to plot for last x episodes

--- eval.py
+++ eval.py
@@ -3,9 +3,10 @@ import seaborn as sns
 import numpy as np
 
 class RLEvaluation:
-    def __init__(self, episode_ticks = 1):
-        self.episodes, self.loss_values, self.score_values = [], [], []
+    def __init__(self, episode_ticks = 1, mean_subset = 100):
+        self.episodes, self.loss_values, self.score_values, self.mean_scores = [], [], [], []
         self.episode_ticks = episode_ticks
+        self.mean_subset = mean_subset
 
         #init plots
         sns.set(style="whitegrid")
@@ -24,7 +25,8 @@ class RLEvaluation:
         plt.pause(0.001)
 
     def plot_score(self):
-        self.score_plot.plot(self.episodes, self.score_values)
+        self.score_plot.plot(self.episodes, self.score_values, color='b', label='score')
+        self.score_plot.plot(self.episodes, self.mean_scores, color='g', label='mean')
         plt.draw()
         plt.pause(0.001)
 
@@ -32,9 +34,11 @@ class RLEvaluation:
         self.episodes.append(episode)
         self.loss_values.append(loss)
         self.score_values.append(score)
+        self.mean_scores.append(np.mean(self.score_values[-self.mean_subset:]))
+
 
         if episode % self.episode_ticks == 0:
             self.plot_score()
             self.plot_train_loss()
 
-            print("Mean Score : {}, total_steps: {}".format(np.mean(self.score_values), np.sum(self.score_values)))
+            print("Mean Score : {}, total_steps: {}".format(np.mean(self.score_values[-self.mean_subset:]), np.sum(self.score_values)))
